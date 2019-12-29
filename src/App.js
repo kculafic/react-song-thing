bb4dbf4ebc3cddb4f56db57e45b7b466926e67b0
Krsto Culafic <kculafic@gmail.com>
2019-12-29 17:44:23 -0600

    Add react guitar chords and configure, style components

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import Track from './Track/Track';
 class App extends Component {
     state = {
         tracks: [
-            { id: 1, title: 'Tamo Daleko Surf Version', writer: 'Krsto', year: 1918, idea_status: 'Loop / Riff', source: 'https://od.lk/s/MTBfOTMwNDc5NDRf/Tamo%20Daleko%20-%20%28serb%20surf%29.mp3'   } ,
-            { id: 2, title: 'Anthem of Yugoslavia', writer: 'Samuel Tomášik', year: 1834, idea_status: 'National Anthem', source: 'https://www.marxists.org/subject/yugoslavia/music/servie-serbian.mp3'   } ,
-            { id: 3, title: 'Airport Mesa', writer: 'Krsto and Joel', year: 2014, idea_status: 'Loop / Riff', source: 'https://od.lk/s/MTBfOTMwNDY2NjNf/airport%20mesa%20%232.mp3'   } ,
-            { id: 4, title: 'Pink Champagne / High Life', writer: 'Krsto', year: 2018, idea_status: 'Loop / Riff from Mixtape', source: 'https://od.lk/s/MTBfOTMwNDkxODFf/Pink%20Champagne%20hig%20Life.m4a'   } ,
+            { id: 1, title: 'Tamo Daleko (Surf Version)', writer: 'Krsto', year: 1918, idea_status: 'Loop / Riff', source: 'https://od.lk/s/MTBfOTMwNDc5NDRf/Tamo%20Daleko%20-%20%28serb%20surf%29.mp3', chords: ['A', 'C', 'D'], key: 'E Major' } ,
+            { id: 2, title: 'Anthem of Yugoslavia', writer: 'Samuel Tomášik', year: 1834, idea_status: 'National Anthem', source: 'https://www.marxists.org/subject/yugoslavia/music/servie-serbian.mp3', chords: ['Amin', 'E', 'G', 'Bmin', 'Fmin'], key: 'E Major' } ,
+            { id: 3, title: 'Airport Mesa', writer: 'Krsto and Joel', year: 2014, idea_status: 'Loop / Riff', source: 'https://od.lk/s/MTBfOTMwNDY2NjNf/airport%20mesa%20%232.mp3', chords: ['Dmin'], key: 'Drop D minor' } ,
+            { id: 4, title: 'Pink Champagne / High Life', writer: 'Krsto', year: 2018, idea_status: 'Loop / Riff from Mixtape', source: 'https://od.lk/s/MTBfOTMwNDkxODFf/Pink%20Champagne%20hig%20Life.m4a', chords: ['A', 'C', 'D'], key: 'E Major' } ,
             // { id: 5, title: 'Shirt', writer: 'Krsto', year: 2015, idea_status: 'Loop / Riff', source: 'www.link.com/981273kjhd'   } ,
             // { id: 6, title: 'Jacket', writer: 'Krsto', year: 2015, idea_status: 'Loop / Riff', source: 'www.link.com/981273kjhd'   } ,
             // { id: 7, title: 'Hat', writer: 'Krsto', year: 2015, idea_status: 'Loop / Riff', source: 'www.link.com/981273kjhd'   } ,
--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
+import GuitarChord from 'react-guitar-chord'
 
 import './Track.css';
 
 
 const Track = props => {
-	console.log(props)
+    console.log(props)
 
-	const track_obj = props.tracks.map((track) =>
-		<div className="track" key={track.id}>
-			<div className="info">
-				<h4>{track.id}: {track.title}</h4>
-				<p>Written by: {track.writer}</p>
-				<p>Status: {track.idea_status}</p>
-				<p>Source: <a href={track.source} >Link</a> </p>
+    const track_obj = props.tracks.map((track) =>
+        <div className="track" key={track.id}>
+            <div className="info">
+                <h4>{track.title}</h4>
+                <p>Written by: {track.writer} in {track.year}</p>
+                <p>Status: {track.idea_status}</p>
+                <p>Source: <a href={track.source} >Link</a> </p>
 
+				<p>Key: {track.key} </p>
+            </div>
+			<div className="chordlist">
+				{track.chords.map(x => {
+					if(x.includes('min')) {
+						console.log(x,'true bitch')
+						let root_note = x.slice(0,1)
+						return <GuitarChord
+									chord={root_note}
+									quality={'MIN'}
+									height={'7em'}
+									/>
+					} else {
+						return <GuitarChord
+									chord={x}
+									height={'7em'}
+									/>
+					}
+				})}
 			</div>
 			<div className="insidebuttons">
 				<ReactAudioPlayer
 					src={track.source}
 					controls
-					width="500px"
+					width="200px"
 				/>
 			</div>
+        </div>
+    );
 
-		</div>
-	);
-
-
-	return (
-			<div className="tracklist">
-				{track_obj}
-			</div>
-
-	);
+    return (
+        <div className="tracklist">
+            {track_obj}
+        </div>
+    );
 
 }
 
